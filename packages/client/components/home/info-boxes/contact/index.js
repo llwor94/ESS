a3a5b5aaa8dd43404beda6349f612356eedcf9d2
Lauren <llwor94@gmail.com>
2019-09-23 19:29:59 -0700

    contact box

--- packages/client/components/home/info-boxes/contact/index.js
+++ packages/client/components/home/info-boxes/contact/index.js
@@ -10,25 +10,45 @@ import '@material/line-ripple/dist/mdc.line-ripple.css';
 import '@material/layout-grid/dist/mdc.layout-grid.css';
 import '@material/button/dist/mdc.button.css'
 import '@rmwc/icon/icon.css';
+import axios from 'axios';
 
 function Contact({styles}) {
+  const [form, setForm] = useState({
+    lastName: undefined,
+    firstName: undefined,
+    phone: undefined,
+    email: undefined,
+    message: undefined
+  });
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value})
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const name = `${form.firstName} ${form.lastName}`;
+    const data = await axios.post('https://us-central1-steel-prism-253608.cloudfunctions.net/email-trigger', {name, phone: form.phone, email: form.email, message: form.message});
+    console.log(data)
+  };
+
   return (
     <div className={clsx(styles.section, styles.contact)}>
       <h2>Contact us</h2>
-      <div className={styles.formbox}>
+      <form onSubmit={handleSubmit} className={styles.formbox}>
         <div className={styles.formrow}>
-          <TextField className={styles.input} outlined label='Student First Name' />
-          <TextField className={styles.input} outlined label='Student Last Name' />
+          <TextField className={styles.input} outlined name='firstName' label='First Name' onChange={handleChange} />
+          <TextField className={styles.input} outlined name='lastName' label='Last Name' onChange={handleChange}  />
         </div>
         <div className={styles.formrow}>
-          <TextField className={styles.input} outlined label='Email' />
-          <TextField className={styles.input} outlined label='Phone Number' />
+          <TextField className={styles.input} outlined name='email' label='Email' onChange={handleChange} />
+          <TextField className={styles.input} outlined name='phone' label='Phone Number' onChange={handleChange}  />
         </div>
         <div className={styles.formrow}>
-        <TextField className={styles.input} textarea rows={8} span={12} maxLength={20} outlined label='Message' />
+        <TextField className={styles.input} textarea rows={8} span={12} maxLength={20} name='message' outlined label='Message' onChange={handleChange} />
         </div>
-        <Button label='Submit' style={{alignSelf: 'flex-end', margin: '10px'}} trailingIcon='send' raised/>
-      </div>
+        <Button disabled={Object.values(form).some(x => x === undefined)} label='Submit' style={{alignSelf: 'flex-end', margin: '10px'}} trailingIcon='send' raised/>
+      </form>
     </div>
   )
 }
